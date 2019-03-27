c89eefcbaa30bc5558bd96d8e6b60d8666163c04
Erick Subieta <ericksubieta@hotmail.com>
2019-03-27 01:08:01 -0400

    Auth with Fingerprint added.

--- app/src/main/java/uagrm/soe/awesomelogin/controller/LoginActivity.kt
+++ app/src/main/java/uagrm/soe/awesomelogin/controller/LoginActivity.kt
@@ -2,12 +2,15 @@ package uagrm.soe.awesomelogin.controller
 
 import android.os.Build
 import android.os.Bundle
+import android.support.v4.hardware.fingerprint.FingerprintManagerCompat
 import android.view.View
+import android.widget.Toast
 import uagrm.soe.awesomelogin.R
 import uagrm.soe.awesomelogin.abstract.AwesomeCompactActivity
+import uagrm.soe.awesomelogin.security.BiometricCallback
 import uagrm.soe.awesomelogin.security.BiometricManager
 
-class LoginActivity : AwesomeCompactActivity() {
+class LoginActivity : AwesomeCompactActivity() , BiometricCallback{
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,9 +23,56 @@ class LoginActivity : AwesomeCompactActivity() {
     }
 
     fun onClickLoginWithBiometric(view: View){
-        var biometricManager = BiometricManager()
-        if (Build.VERSION.SDK_INT >= 28) {
-            biometricManager.createBiometricInstance(this)
-        }
+        BiometricManager.BiometricBuilder(MainActivity@this)
+                        .setTitle(getString(R.string.biometric_title))
+                        .setSubtitle(getString(R.string.biometric_subtitle))
+                        .setDescription(getString(R.string.biometric_description))
+                        .setNegativeButtonText(getString(R.string.biometric_negative_button_text))
+                        .build()
+                        .authenticate(MainActivity@this)
+    }
+
+
+
+
+    override fun onSdkVersionNotSupported() {
+        Toast.makeText(getApplicationContext(), getString(R.string.biometric_error_sdk_not_supported), Toast.LENGTH_LONG).show();
+    }
+
+
+    override fun onBiometricAuthenticationNotSupported() {
+        Toast.makeText(getApplicationContext(), getString(R.string.biometric_error_hardware_not_supported), Toast.LENGTH_LONG).show();
+    }
+
+    override fun onBiometricAuthenticationNotAvailable() {
+        Toast.makeText(getApplicationContext(), getString(R.string.biometric_error_fingerprint_not_available), Toast.LENGTH_LONG).show();
+    }
+
+    override fun onBiometricAuthenticationPermissionNotGranted() {
+        Toast.makeText(getApplicationContext(), getString(R.string.biometric_error_permission_not_granted), Toast.LENGTH_LONG).show();
+    }
+
+    override fun onBiometricAuthenticationInternalError(error: String) {
+        Toast.makeText(getApplicationContext(), error, Toast.LENGTH_LONG).show();
+    }
+
+    override fun onAuthenticationFailed() {
+        Toast.makeText(getApplicationContext(), getString(R.string.biometric_failure), Toast.LENGTH_LONG).show();
+    }
+
+    override fun onAuthenticationCancelled() {
+        Toast.makeText(getApplicationContext(), getString(R.string.biometric_cancelled), Toast.LENGTH_LONG).show();
+    }
+
+    override fun onAuthenticationSuccessful() {
+        Toast.makeText(getApplicationContext(), getString(R.string.biometric_success), Toast.LENGTH_LONG).show();
+    }
+
+    override fun onAuthenticationHelp(helpCode: Int,  helpString: CharSequence) {
+        Toast.makeText(getApplicationContext(), helpString, Toast.LENGTH_LONG).show();
+    }
+
+    override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
+        Toast.makeText(getApplicationContext(), errString, Toast.LENGTH_LONG).show();
     }
 }
---
+++ app/src/main/java/uagrm/soe/awesomelogin/security/BiometricDialogV23.kt
@@ -0,0 +1,101 @@
+package uagrm.soe.awesomelogin.security
+
+import android.content.Context
+import android.content.DialogInterface
+import android.support.design.widget.BottomSheetDialog
+import android.view.View
+import android.graphics.drawable.Drawable
+import uagrm.soe.awesomelogin.R.id.item_description
+import uagrm.soe.awesomelogin.R.id.item_subtitle
+import uagrm.soe.awesomelogin.R.id.item_status
+import uagrm.soe.awesomelogin.R.id.item_title
+import uagrm.soe.awesomelogin.R.id.img_logo
+import uagrm.soe.awesomelogin.R.id.btn_cancel
+import uagrm.soe.awesomelogin.R.layout.view_bottom_sheet
+import android.support.annotation.NonNull
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import uagrm.soe.awesomelogin.R
+
+
+class BiometricDialogV23 : BottomSheetDialog, View.OnClickListener {
+
+    private var btnCancel: Button? = null
+    private var imgLogo: ImageView? = null
+    private var itemTitle: TextView? = null
+    private var itemDescription:TextView? = null
+    private var itemSubtitle:TextView? = null
+    private var itemStatus:TextView? = null
+
+    private var biometricCallback: BiometricCallback? = null
+
+
+    constructor(context: Context, biometricCallback: BiometricCallback) : super(context) {
+        this.biometricCallback = biometricCallback
+    }
+
+    constructor(context: Context, theme: Int, biometricCallback: BiometricCallback) : super(context, theme) {
+        this.biometricCallback = biometricCallback
+    }
+
+    constructor(context: Context, cancelable: Boolean, cancelListener: DialogInterface.OnCancelListener?, biometricCallback: BiometricCallback) : super(context, cancelable, cancelListener) {
+        this.biometricCallback = biometricCallback
+    }
+
+    constructor(context: Context) : super(context)
+
+
+    private fun setDialogView() {
+        val bottomSheetView = layoutInflater.inflate(R.layout.view_bottom_sheet, null)
+        setContentView(bottomSheetView)
+
+        btnCancel = findViewById(R.id.btn_cancel)
+        btnCancel!!.setOnClickListener(this)
+
+        imgLogo = findViewById(R.id.img_logo)
+        itemTitle = findViewById(R.id.item_title)
+        itemStatus = findViewById(R.id.item_status)
+        itemSubtitle = findViewById(R.id.item_subtitle)
+        itemDescription = findViewById(R.id.item_description)
+
+        updateLogo()
+    }
+
+    fun setTitle(title: String) {
+        itemTitle!!.text = title
+    }
+
+    fun updateStatus(status: String) {
+        itemStatus!!.setText(status)
+    }
+
+    fun setSubtitle(subtitle: String) {
+        itemSubtitle!!.setText(subtitle)
+    }
+
+    fun setDescription(description: String) {
+        itemDescription!!.setText(description)
+    }
+
+    fun setButtonText(negativeButtonText: String) {
+        btnCancel!!.setText(negativeButtonText)
+    }
+
+    private fun updateLogo() {
+        try {
+            val drawable = getContext().packageManager.getApplicationIcon(context!!.getPackageName())
+            imgLogo!!.setImageDrawable(drawable)
+
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
+    }
+
+
+    override fun onClick(view: View) {
+        dismiss()
+        biometricCallback!!.onAuthenticationCancelled()
+    }
+}
